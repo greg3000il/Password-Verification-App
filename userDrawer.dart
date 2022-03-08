153dfa00bf8202889064d1a6e1e044efcc5e3d74
greg3000il <77967220+greg3000il@users.noreply.github.com>
2022-03-08 09:27:07 +0200

    Add files via upload

---
+++ userDrawer.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:password_verifications_app/Screens/getUsers.dart';
+import 'package:password_verifications_app/Screens/registerUser.dart';
+
+class userDrawer extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return userDrawerState();
+  }
+}
+
+class userDrawerState extends State<userDrawer> {
+  final minimumPadding = 5.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("User Management"),
+      ),
+      body: Center(
+        child: Text(
+            "Welcome to the app, Press the drawer on the top left to see more options"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.only(top: minimumPadding, bottom: minimumPadding),
+          children: <Widget>[
+            DrawerHeader(
+              child: Text("User Mangment"),
+              decoration: BoxDecoration(color: Colors.blue),
+            ),
+            ListTile(
+              title: Text("Register User"),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => registerUser()));
+              },
+            ),
+            ListTile(
+              title: Text("Get Users"),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => getusers()));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
